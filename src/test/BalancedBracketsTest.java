48a395da432e99b3f7a5121175499544152dee58
ramadevimaram <ramamaram.java@gmail.com>
2020-07-02 20:43:07 -0400

    Testclasses added

--- src/test/BalancedBracketsTest.java
+++ src/test/BalancedBracketsTest.java
@@ -1,16 +1,36 @@
 package test;
 
+import main.BalancedBrackets;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
 
 public class BalancedBracketsTest {
 
-    //TODO: add tests here
+    /*//TODO: add tests here
     @Test
     public void emptyTest() {
         assertEquals(true, true);
     }
-
-
+    */
+    @Test
+    public void onlyBracketsReturnsTrue() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[]"));
+    }
+    @Test
+    public void oddBracketsReturnsFalse(){
+        assertFalse(BalancedBrackets.hasBalancedBrackets("]someString[]"));
+    }
+    @Test
+    public void assertEqualExample(){
+        assertEquals(false,BalancedBrackets.hasBalancedBrackets("]someString[]"));
+    }
+    @Test
+    public void returnSpecificString(){
+        assertSame(BalancedBrackets.hasBalancedBrackets("[]"), true);
+    }
+    @Test
+    public void notNullExample(){
+        assertNotNull(BalancedBrackets.hasBalancedBrackets("]someString[]"));
+    }
 }
