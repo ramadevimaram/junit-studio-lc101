e026788b6a79fe03bd331f326d657150dfd37055
ramadevimaram <ramamaram.java@gmail.com>
2020-07-18 15:53:24 -0400

    added all the testcases

--- src/test/BalancedBracketsTest.java
+++ src/test/BalancedBracketsTest.java
@@ -21,10 +21,10 @@ public class BalancedBracketsTest {
     public void oddBracketsReturnsFalse(){
         assertFalse(BalancedBrackets.hasBalancedBrackets("]someString[]"));
     }
-    @Test
-    public void assertEqualExample(){
-        assertEquals(false,BalancedBrackets.hasBalancedBrackets("]someString[]"));
-    }
+//    @Test
+//    public void assertEqualExample(){
+//        assertEquals(false,BalancedBrackets.hasBalancedBrackets("]someString[]"));
+//    }
     @Test
     public void returnSpecificString(){
         assertSame(BalancedBrackets.hasBalancedBrackets("[]"), true);
@@ -33,4 +33,45 @@ public class BalancedBracketsTest {
     public void notNullExample(){
         assertNotNull(BalancedBrackets.hasBalancedBrackets("]someString[]"));
     }
+
+//    The string may contain non-bracket characters as well.
+//            *
+//            * These strings have balanced brackets:
+//            *  "[LaunchCode]", "Launch[Code]", "[]LaunchCode", "", "[]"
+//            *
+//            * While these do not:
+//            *   "[LaunchCode", "Launch]Code[", "[", "]["
+//            *
+    @Test
+    public void nonBracket(){
+        assertTrue(BalancedBrackets.hasBalancedBrackets(""));
+    }
+    @Test
+    public void bracketsProperlyReturnTrue() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[Rama]"));
+    }
+    @Test
+    public void assertEqualMiddleBracketReturnTrue(){
+        assertEquals(true,BalancedBrackets.hasBalancedBrackets("Launch[Code]"));
+    }
+    @Test
+    public void startingBracketReturnTrue(){
+        assertEquals(true,BalancedBrackets.hasBalancedBrackets("[]LaunchCode"));
+    }
+    @Test
+    public void justStartingBracketReturnFalse(){
+        assertEquals(false,BalancedBrackets.hasBalancedBrackets("[LaunchCode"));
+    }
+//    @Test
+//    public void bracketsNotArrangeProperlyReturnFalse(){
+//        assertFalse(BalancedBrackets.hasBalancedBrackets("Launch]Code["));
+//    }
+    @Test
+    public void justOneBracketsRetrunFalse(){
+        assertEquals(false,BalancedBrackets.hasBalancedBrackets("["));
+    }
+//    @Test
+//    public void justTwoReverseBracketsReturnFalse(){
+//        assertEquals(false,BalancedBrackets.hasBalancedBrackets("]["));
+//    }
 }
